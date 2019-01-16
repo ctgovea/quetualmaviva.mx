19a723fe784dfcd28cdaf193b5e2190d84f7cc82
govea <ctgovea@gmail.com>
2019-01-15 18:36:19 -0600

    merged js into app.js

--- scripts/app.js
+++ scripts/app.js
@@ -18,4 +18,98 @@ $(document).ready(function () {
       $('#image-gallery').removeClass('cS-hidden');
     }
   });
+
+  const subscribe_URL = 'https://eovsmc4uu6.execute-api.us-east-1.amazonaws.com/dev/api/subscribe';
+  const register_URL = 'https://eovsmc4uu6.execute-api.us-east-1.amazonaws.com/dev/api/register';
+
+  // Signup
+
+  $('#signupForm').submit(function (e) {
+    e.preventDefault();
+
+    let $form = $(this);
+    if (!$form.valid()) return false;
+
+    $submit = $('#signupForm [type="submit"]');
+    $submit.prop('disabled', true);
+
+    const $msg = $('#signupMsg');
+
+    const email = document.getElementById('emailSignup').value;
+    var data = JSON.stringify({
+      email
+    });
+
+    $.ajax({
+      type: 'POST',
+      url: subscribe_URL,
+      data: data,
+      contentType: 'application/json',
+      crossDomain: true,
+      success: function (response) {
+        $msg.html('<i class="fa fa-check" style="padding-right:5px"></i>¡Listo! Gracias por suscribirte.')
+          .addClass('success')
+          .removeClass('error')
+          .slideDown();
+        $form.hide();
+      },
+      error: function (response) {
+        $msg.text(':( algo salió mal. Intenta de nuevo.')
+          .addClass('error')
+          .slideDown();
+        $submit.prop('disabled', false);
+      }
+    });
+  });
+
+  // Registration 
+
+  $('#registerForm').validate({
+    messages: {
+      'name': 'Falta que nos digas tu nombre',
+      'email': 'Escribe un correo electrónico válido',
+    }
+  });
+
+  $('#registerForm').submit(function (e) {
+    e.preventDefault();
+    let form = $(this);
+    if (!form.valid()) return false;
+
+    $submitButton = $('#submit');
+    $submitButton.val("Enviando...");
+
+    $statusMessage = $('#formStatusMessage');
+    $statusMessage.hide();
+
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const phone = document.getElementById('phone').value;
+    const comments = document.getElementById('comments').value;
+
+    var data = JSON.stringify({
+      name,
+      email,
+      phone,
+      comments
+    });
+
+    $.ajax({
+      type: 'POST',
+      url: register_URL,
+      data: data,
+      contentType: 'application/json',
+      crossDomain: true,
+      success: function (response) {
+        console.log(response);
+        $statusMessage.text('¡Gracias! Tu registro fue enviado.')
+          .slideDown();
+        $submitButton.hide();
+      },
+      error: function (response) {
+        $statusMessage.text(':( algo salió mal. Intenta de nuevo.')
+          .slideDown();
+      }
+    });
+  });
 });
