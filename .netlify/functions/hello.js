a9081535726139e2a3daca3315619ff92e85f850
govea <ctgovea@gmail.com>
2020-02-22 16:45:26 -0600

    only POST allowed

--- .netlify/functions/hello.js
+++ .netlify/functions/hello.js
@@ -1,4 +1,8 @@
 exports.handler = async (event, context) => {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
   return {
     statusCode: 200,
     body: "Gracias por registrarte"
